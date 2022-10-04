81f028cbd5702ee08b534e8797c43940b125f364
Sam Avery <sam.avery1@gmail.com>
2022-10-04 14:54:35 -0500

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
 //set up routing, add other components 
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import NavBar from './NavBar';
 import Home from './Home';
 import ArticleList from './ArticleList'
 import ResourceList from './ResourceList';
 import BookList from './BookList'
 import Footer from './Footer'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
-
   return (
     <div>
       <header>
-        <NavBar />
+        <NavBar/>
         <h1 className='page-title'>Book Deserts <br /> and who's helping to end them</h1>
-        <Home />
-        <ArticleList />
-        <BookList />
-        <ResourceList />
+        <Switch>
+          <Route exact path="/articles">
+            <ArticleList />
+          </Route>
+          <Route exact path="/resources">
+            <ResourceList />
+          </Route>
+          <Route exact path="/books">
+            <BookList />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <h1>404 not found</h1>
+          </Route>
+          </Switch>
       </header>
       <Footer />
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,15 @@
 // should be top of every page, add to App 
 
 import React from "react";
-import { NavLink, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
+
   return (<div className="navbar">
-    HOME
-    ARTICLES
-    RESOURCES 
-    BOOKS 
-        {/* <NavLink
-          to="/"
-          exact
-          >
-          Home
-          </NavLink> */}
-          {/* <NavLink 
-            to="/articles"
-            exact
-            >
-            Articles
-            </NavLink>
-            <NavLink 
-              to="/donate-resources"
-              exact
-            >
-              Donations + Resources
-            </NavLink>
-            <NavLink 
-              to="/books"
-              exact
-            >
-              Books
-            </NavLink> */}
+    <NavLink to="/" exact>Home </NavLink>
+    <NavLink to="/articles" exact>Articles </NavLink>
+    <NavLink to="/resources" exact>Resources </NavLink>
+    <NavLink to="/books" exact>Books </NavLink>
   </div>);
 }
 
