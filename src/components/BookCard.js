72911c55ae04028a3bc67a37f7a1c07f993f4ff2
Sam Avery <sam.avery1@gmail.com>
2022-10-06 16:54:13 -0500

    delete styling

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-function BookCard({id, title, image, author, url, handleRemoveListing, books}) {
+function BookCard({id, title, image, author, url, handleRemoveBook, books}) {
     
     function handleDeleteClick() {
         fetch(`http://localhost:3000/books/${id}`, {
           method: "DELETE",
         });
-        handleRemoveListing(id);
-        console.log(id)
+        handleRemoveBook(id);
       }
-    console.log(id)
+
     return (
         <div className='card'>
-            <button onClick={handleDeleteClick} className="delete-btn">X</button>
             <h1>{title}</h1>
             {image ?  <img src={image} alt={title}/> : null}
             <h3>By: {author}</h3>
             <a target="_blank" href={url} >Read more (or buy one to donate!)</a>
+            <br></br>
+            <br></br>
+            <button onClick={handleDeleteClick} className="delete-btn">Click to remove from list</button>
         </div>
     )
 }
